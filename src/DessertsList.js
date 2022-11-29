ce628221db08b3be8a282c3bb1192a11f9836017
Cascaval Nadejda <vremea.dr@gmail.com>
2022-11-29 19:12:50 +0000

    Edit dessert list

--- src/DessertsList.js
+++ src/DessertsList.js
@@ -1,18 +1,20 @@
 function DessertsList({data}) {
   // Implement the component here.
 
-  const sortedDesserts = data.sort((a, b) => {
-    return b.calories - a.calories
-  })
-
-  const filteredDesserts = sortedDesserts.filter((item) => item.calories < 500)
-
-  const displayDesserts = filteredDesserts.map((item, index) => {
-    return <li key={index}>{`${item.name} - ${item.calories} cal`}</li>
-  })
+  const sortedData = data
+    .filter(item => item.calories < 500)
+    .sort((a,b) => {
+      return a.calories - b.calories
+    })
+    .map((dessert, index) => {
+      return (
+        <li key={index}>{`${dessert.name} - ${dessert.calories} cal`}</li>
+      )
+    })
+    
   return (
     <ul>
-      {displayDesserts}
+      {sortedData}
     </ul>
   )
 }
