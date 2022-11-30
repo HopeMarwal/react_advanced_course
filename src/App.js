6257043d66f700319810f8c99e8df0771454c171
Cascaval Nadejda <vremea.dr@gmail.com>
2022-11-30 20:53:40 +0000

    Managing stata within a component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import "./App.css";
 //Components
 import Form from "./components/Form";
 import Theme from "./components/Theme";
+import ManageState from "./components/ManageState";
 
 
 function App() {
 
   return (
     <div className="App">
-      <Theme />
+      <ManageState />
     </div>
   );
 }
