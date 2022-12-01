aa2d1583e0729612c18925bcfb84d06c76f02f54
Cascaval Nadejda <vremea.dr@gmail.com>
2022-12-01 18:54:12 +0000

    Initialize FetchingData component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import "./App.css";
 //Components
-import Form from "./components/Form";
-import Theme from "./components/Theme";
-import ManageState from "./components/ManageState";
+// import Form from "./components/Form";
+// import Theme from "./components/Theme";
+// import ManageState from "./components/ManageState";
+import FetchingData from "./components/FetchingData";
 
 
 function App() {
 
   return (
     <div className="App">
-      <ManageState />
+      <FetchingData />
     </div>
   );
 }
---
+++ src/components/FetchingData.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function FetchingData() {
+  return (
+    <div>FetchingData</div>
+  )
+}
