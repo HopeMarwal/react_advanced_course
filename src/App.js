3b96bfb588d27f5c93860c505250180f1ba69a16
Cascaval Nadejda <vremea.dr@gmail.com>
2022-12-06 11:59:02 +0000

    RadioGroup

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
+import { useState } from "react";
 import "./App.css";
 //Components
 // import Form from "./components/Form";
 // import Theme from "./components/Theme";
 // import ManageState from "./components/ManageState";
 //import FetchingData from "./components/FetchingData";
-import Test from "./components/Test";
+//import Test from "./components/Test";
+import {RadioGroup, RadioOption} from "./components/Radio/RadioGroup";
 
 
 function App() {
 
+  const [selected, setSelected] = useState("");
+
   return (
     <div className="App">
-      <Test />
+       <h2>How did you hear about Little Lemon?</h2>
+      <RadioGroup onChange={setSelected} selected={selected}>
+        <RadioOption value="social_media">Social Media</RadioOption>
+        <RadioOption value="friends">Friends</RadioOption>
+        <RadioOption value="advertising">Advertising</RadioOption>
+        <RadioOption value="other">Other</RadioOption>
+      </RadioGroup>
+      <button disabled={!selected}>Submit</button>
     </div>
   );
 }
---
+++ src/components/Radio/RadioGroup.jsx
@@ -0,0 +1,28 @@
+import React, { cloneElement } from "react";
+import "./styles.css";
+
+export const RadioGroup = ({ onChange, selected, children }) => {
+  // Use React.Children.map and React.cloneElement to clone the children
+  React.Children.map(children, (item) => {
+    return cloneElement(item, {
+      onChange,
+      checked: item.props.value
+    })
+
+  })
+  // and pass the correct props to each RadioOption
+  const RadioOptions = null;
+
+  return <div className="RadioGroup">{RadioOptions}</div>;
+};
+
+export const RadioOption = ({ value, checked, onChange, children }) => {
+  // Hook up the onChange handler to call the onChange prop passed to RadioGroup
+  // Also, make sure to pass the correct checked prop to the input element
+  return (
+    <div className="RadioOption">
+      <input id={value} type="radio" name={value} />
+      <label htmlFor={value}>{children}</label>
+    </div>
+  );
+};
