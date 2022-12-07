de4fa144ea2cc91c9dc731710a47abbd6beb2003
Cascaval Nadejda <vremea.dr@gmail.com>
2022-12-07 10:27:54 +0000

    Comment

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import "./App.css";
 // import ManageState from "./components/ManageState";
 //import FetchingData from "./components/FetchingData";
 //import Test from "./components/Test";
-import {RadioGroup, RadioOption} from "./components/Radio";
+//import {RadioGroup, RadioOption} from "./components/Radio";
 
 
 function App() {
@@ -14,19 +14,21 @@ function App() {
   const [selected, setSelected] = useState("");
 
   return (
-    <div className="App">
-      <h2>How did you hear about Little Lemon?</h2>
+    <>
+    </>
+    // <div className="App">
+    //   <h2>How did you hear about Little Lemon?</h2>
 
-      <RadioGroup onChange={setSelected} selected={selected}>
-        <RadioOption value="social_media">Social Media</RadioOption>
-        <RadioOption value="friends">Friends</RadioOption>
-        <RadioOption value="advertising">Advertising</RadioOption>
-        <RadioOption value="other">Other</RadioOption>
-      </RadioGroup>
+    //   <RadioGroup onChange={setSelected} selected={selected}>
+    //     <RadioOption value="social_media">Social Media</RadioOption>
+    //     <RadioOption value="friends">Friends</RadioOption>
+    //     <RadioOption value="advertising">Advertising</RadioOption>
+    //     <RadioOption value="other">Other</RadioOption>
+    //   </RadioGroup>
 
-      <button disabled={!selected}>Submit</button>
+    //   <button disabled={!selected}>Submit</button>
 
-    </div>
+    // </div>
   );
 }
 
